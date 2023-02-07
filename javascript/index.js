58e98ba4468b10ba901ee130ca9b00a14468d864
HIN <114932705+HIM003l@users.noreply.github.com>
2023-02-07 16:09:29 -0300

    aplicação do carrossel de lançamentos

--- javascript/index.js
+++ javascript/index.js
@@ -5,3 +5,19 @@ menu.onclick = function () {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+ nextImage();
+},4000)
+
+function nextImage(){
+    count++;
+    if(count > 4){
+     count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
